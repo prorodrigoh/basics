27adeae07efc4f8727b06e098acd62a4fbbc315b
ROD <104014121+prorodrigoh@users.noreply.github.com>
2022-04-21 11:57:03 -0400

    added code for chicken and egg

---
+++ async.js
@@ -0,0 +1,12 @@
+console.log('This comes first. 🥚')
+
+setTimeout(() => {
+    console.log('This is in the middle...')
+}, 0)
+print()
+console.log('This is in the end. 🐓')
+
+function print (){
+    console.log('Here now')
+}
+
