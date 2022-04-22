4059f6b92e031f54b7b849b32cc9473b87de3c6f
ROD <104014121+prorodrigoh@users.noreply.github.com>
2022-04-22 15:56:35 -0400

    solved fetch problem to take the data outside by crating a function outside and calling it inside the fetch

--- asyncJon.js
+++ asyncJon.js
@@ -1,32 +1,35 @@
 import fetch from 'node-fetch'
 
-function goGetMyBeverages (){
+// function goGetMyBeverages (){
 
-    let allBevs
-    let allBeers    
-    let allCoffees
+//     let allBevs =[]
+//     let allBeers    
+//     let allCoffees
 
-    fetch('https://api.sampleapis.com/beers/ale')
-        .then(rawBeer => rawBeer.json())
-        .then(jsonBeer => {
-            allBeers = jsonBeer
-            allBevs = Object.assign(allBeers)
-            console.log(allBevs.length)
-        })
-        .catch(myErr => console.log('Error found:', myErr))
+//         fetch('https://api.sampleapis.com/beers/ale')
+//             .then(rawBeer => rawBeer.json())
+//             .then(jsonBeer => {
+//                 allBeers = jsonBeer
+//                 allBevs = Object.assign(allBeers)
+//                 console.log(allBevs.length)
+//             })
+//             .catch(myErr => console.log('Error found:', myErr))
 
-    fetch('https://api.sampleapis.com/coffee/hot')
-        .then(rawCoffee => rawCoffee.json())
-        .then(jsonCoffee => {
-            allCoffees = jsonCoffee
-            allBevs = Object.assign(allCoffees)
-            console.log(allBevs.length)
-        })
-        .catch(myErr => console.log('Error found:', myErr))
+
+//         fetch('https://api.sampleapis.com/coffee/hot')
+//             .then(rawCoffee => rawCoffee.json())
+//             .then(jsonCoffee => {
+//                 allCoffees = jsonCoffee
+//                 allBevs = Object.assign(allCoffees)
+//                 console.log(allBevs.length)
+//             })
+//             .catch(myErr => console.log('Error found:', myErr))
+
     
-}
+//     console.log(allBevs)
+// }
 
-goGetMyBeverages()
+// goGetMyBeverages()
 
 // criar funcao para aceitar o endereco do fetch
 // retornar os dados para fora da funcao
@@ -35,3 +38,22 @@ goGetMyBeverages()
 // executar com o primeiro endereco e retornar para o arquivo geral
 // esperar a execucao e rodar outra vez com o outro endereco e retornar para o arquivo geral
 // retornar o arquivo geral
+
+
+function goGetBeverages (apiUrl){
+  fetch(apiUrl)
+        .then(raw => raw.json())
+        .then(data => getAllBeverages(data[0]))
+        .catch(myErr => console.log('Error found:', myErr))
+}
+let allBevs = []
+let getAllBeverages = (data) => {
+  allBevs.push(data)
+  console.log(allBevs.length)
+}
+
+const beerUrl = 'https://api.sampleapis.com/beers/ale'
+const coffeeUrl = 'https://api.sampleapis.com/coffee/hot'
+
+goGetBeverages(beerUrl)
+goGetBeverages(coffeeUrl)
