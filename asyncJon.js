1d8491b5cfb1532b6af88626e00e228380f0fbcd
ROD <104014121+prorodrigoh@users.noreply.github.com>
2022-04-21 22:40:00 -0400

    async de dois arquivos

---
+++ asyncJon.js
@@ -0,0 +1,37 @@
+import fetch from 'node-fetch'
+
+function goGetMyBeverages (){
+
+    let allBevs
+    let allBeers    
+    let allCoffees
+
+    fetch('https://api.sampleapis.com/beers/ale')
+        .then(rawBeer => rawBeer.json())
+        .then(jsonBeer => {
+            allBeers = jsonBeer
+            allBevs = Object.assign(allBeers)
+            console.log(allBevs.length)
+        })
+        .catch(myErr => console.log('Error found:', myErr))
+
+    fetch('https://api.sampleapis.com/coffee/hot')
+        .then(rawCoffee => rawCoffee.json())
+        .then(jsonCoffee => {
+            allCoffees = jsonCoffee
+            allBevs = Object.assign(allCoffees)
+            console.log(allBevs.length)
+        })
+        .catch(myErr => console.log('Error found:', myErr))
+    
+}
+
+goGetMyBeverages()
+
+// criar funcao para aceitar o endereco do fetch
+// retornar os dados para fora da funcao
+
+// criar funcao async para executar a funcao do fetch
+// executar com o primeiro endereco e retornar para o arquivo geral
+// esperar a execucao e rodar outra vez com o outro endereco e retornar para o arquivo geral
+// retornar o arquivo geral
