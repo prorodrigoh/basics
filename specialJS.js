0acbe7575183352e3cb18bb26c9a548619d1021f
ROD <104014121+prorodrigoh@users.noreply.github.com>
2022-04-25 09:00:40 -0400

    Special functions for JS

---
+++ specialJS.js
@@ -0,0 +1,52 @@
+
+// const person = Object.freeze({
+//     name: 'Rod',
+//     age: '44',
+//     food: 'pizza',
+// })
+
+// console.log(person)
+// person.name = 'Rodrigo'
+// console.log(person)
+
+// ---
+
+// const currency_map = new Map ([])
+// const usa = {name: "United States", continent: "North America"}
+// currency_map.set(usa, "USD")
+// console.log(currency_map.get(usa))
+
+// ---
+
+// const unique = [1, 234, 234, 5]
+// const set = new Set(unique)
+// console.log(set)
+// console.log(set.has(1))
+// console.log(set.add(4))
+// console.log(set.delete(5))
+
+// ---
+
+// console.time("timer")
+// for(i=0; i<1000000000; i++){
+//     //This is a long loop
+// }
+// console.timeEnd("timer")
+
+
+// ---
+
+// const x = 2
+// if (x !== 1) console.log("case x is not 1, this will log out")
+// console.assert(x===1, "case x is not 1, this will log out")
+
+// ---
+
+// const people = [
+//     {name: 'Rod', age: '44'},
+//     {name: 'Nat', age: '42'},
+//     {name: 'Chq', age: '10'},
+//     {name: 'Khq', age: '6'},
+// ]
+// // debugger
+// console.table(people)
